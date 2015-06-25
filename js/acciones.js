890c0bd6d9f08cb068dda3faaf90b4983b5361a4
AL02F13 <damaris.aniceto.marin@gmail.com>
2015-06-25 09:43:37 -0500

    uno
    proyecto aplicacion movil de 5 temas mios

---
+++ js/acciones.js
@@ -0,0 +1,75 @@
+// JavaScript Document
+$(document).ready(function(e) {
+document.addEventListener("deviceready",function(){
+
+$('#btntemporadas').on ('tap', function() {
+	navigator.vibrate(1000);
+	
+	
+}); 
+
+
+$('#btntemporada1').on ('tap', function() {
+	navigator.vibrate(1000);
+	
+	
+}); 
+
+$('#btntemporada2').on ('tap', function() {
+	navigator.vibrate(1000);
+	
+	
+}); 
+
+$('#btntemporada3').on ('tap', function() {
+	navigator.vibrate(1000);
+	
+	
+});
+
+$('#btntemporada4').on ('tap', function() {
+	navigator.vibrate(1000);
+	
+	
+});
+
+$('#btntemporada5').on ('tap', function() {
+	navigator.vibrate(1000);
+	
+	
+	});
+	
+$('#btn_home00').on ('tap', function() {
+	navigator.vibrate(1000);
+	
+	
+}); 
+
+
+$('#btn_home01').on ('tap', function() {
+	navigator.vibrate(1000);
+	
+	
+}); 
+
+$('#btn_home02').on ('tap', function() {
+	navigator.vibrate(1000);
+	
+	
+}); 
+
+$('#btn_home03').on ('tap', function() {
+	navigator.vibrate(1000);
+	
+	
+});
+
+$('#btn_home04').on ('tap', function() {
+	navigator.vibrate(1000);
+	
+	
+});
+
+}); 
+});
+
